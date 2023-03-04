758ea65d5711cb6ef8ea43e93e502e2f15d51224
mdnumanahmed <mnumansyl@gmail.com>
2023-03-05 01:05:10 +0600

    execution context and call stack noted

---
+++ Module-39-More-about-JavaScript/js-engine.js
@@ -0,0 +1,7 @@
+/* 
+V8 Engine 
+When load any JavaScript file on the chrome browser, V8 Engine parse the code line by line. and make a abstract syntax tree, then compile the code via JIT and transfer it as Byte code then machine code. finally execute the code. 
+
+V8 Engine is a open source and made by google. 
+
+*/
