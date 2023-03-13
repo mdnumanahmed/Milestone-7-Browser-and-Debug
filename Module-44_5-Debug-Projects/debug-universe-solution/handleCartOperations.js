98ecb38ef683726c7f2115d5c771d056f25316f7
mdnumanahmed <mnumansyl@gmail.com>
2023-03-14 01:25:23 +0600

    conceptual session project debug universe complated

--- Module-44_5-Debug-Projects/debug-universe-solution/handleCartOperations.js
+++ Module-44_5-Debug-Projects/debug-universe-solution/handleCartOperations.js
@@ -3,10 +3,12 @@ const addToCart = async (id) => {
   const result = await data.json();
   const { name, summary, property_type, images,number_of_reviews,price,_id} = result.find((item) => item._id == id);
   const cartItems=getItemsFromStorage()
-  
-  
+  if(cartItems.find(item => item._id == id)){
+    return
+  }  
   cartItems.push({ name, summary, property_type, images,number_of_reviews,price,_id});
-  localStorage.setItem('saved-Cart', JSON.stringify(cartItems))
+  localStorage.setItem('savedCart', JSON.stringify(cartItems))
+  displayCartItems()
   //const cartItemsContainer = document.getElementById("cart-items");
 };
 
@@ -14,7 +16,7 @@ const getItemsFromStorage = () => {
   let itemsArray = [];
   const cartItems = localStorage.getItem("savedCart");
   if (cartItems) {
-    itemsArray = (cartItems);
+    itemsArray = JSON.parse(cartItems);
   }
   return itemsArray;
 };
@@ -30,10 +32,10 @@ const displayCartItems=()=>{
         cartItemsContainer.innerHTML += `
         <tr>
         <th scope="row">${name.slice(0,26)}</th>
-        <td><span> <i onclick='deleteItemFromCart(${_id})' class="mx-2 bi bi-trash3 text-danger"></i>
+        <td><span> <i onclick="deleteItemFromCart('${_id}')" class="mx-2 bi bi-trash3 text-danger"></i>
         </span> 
         <span> 
-        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo(${_id})' data-bs-toggle="modal" data-bs-target="#paymenModal" ></i> 
+        <i class="text-success bi bi-credit-card-fill" onclick='handlePaymentInfo("${_id}")' data-bs-toggle="modal" data-bs-target="#paymentModal" ></i> 
         </span></td>
        
         </tr>
@@ -46,7 +48,7 @@ displayCartItems()
 
 const deleteItemFromCart=(id)=>{
     const cartItems=getItemsFromStorage()
-    const filteredItems=cartItems.filter((item)=>item._id==id)
+    const filteredItems=cartItems.filter((item)=>item._id !== id)
     localStorage.setItem('savedCart', JSON.stringify(filteredItems))
     displayCartItems()
 }
--- Module-44_5-Debug-Projects/debug-universe-solution/taglineGenarator.js
+++ Module-44_5-Debug-Projects/debug-universe-solution/taglineGenarator.js
@@ -15,11 +15,11 @@ function generateTag(arr, interval) {
   setInterval(() => {
     const randomIndex = Math.floor(Math.random() * arr.length);
     const randomTagLine = tagLines[randomIndex];
-    document.getElementById("taglines").innerText = randomTagLine;
+    document.getElementById("tagline").innerText = randomTagLine;
   }, interval);
 }
 
-generateTag(tagLines, 2000);
+generateTag(tagLines, 3000);
 
 document
   .getElementById("random-room-btn")
@@ -29,15 +29,16 @@ document
     const result = await data.json();
     const randomIndex = Math.floor(Math.random() * 10);
     const modalBody = document.getElementById("random-room-info-modal-body");
-    const { name, summary, property_type, images, review_scores } = result[9];
+    const { name, summary, property_type, images, review_scores } = result[randomIndex];
     
     
 
     const reviews = document.createElement("ol");
     if (review_scores.scores) {
+      console.log(review_scores.scores);
       reviews.setAttribute('class',"list-group")
 
-      object.keys(review_scores.scores).forEach((key) => {
+      Object.keys(review_scores.scores).forEach((key) => {
         reviews.innerHTML += `
         <li class="list-group-item list-group-item-info d-flex justify-content-between align-items-center">
         ${key} : 
@@ -49,7 +50,7 @@ document
       reviews.innerHTML = "No reviews found";
     }
 
-    
+
     modalBody.innerHTML = `
     <div class="col">
     <div class="card h-100">
@@ -61,7 +62,8 @@ document
         <p class="card-text">${summary}</p>
         <div id='review-score'>
       Review Scores :
-      ${review_scores.scores.review_scores_accuracy}
+      <div id="review-scores"></div>
+      
     </div>
       </div>
       <button class="btn btn-info btn-lg"  role="button"
@@ -70,6 +72,7 @@ document
    </div>
     `;
     
+    document.getElementById('review-scores').appendChild(reviews)
   });
 
 
